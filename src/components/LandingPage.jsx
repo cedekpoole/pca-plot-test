0630e73268683917df722ad88a741e23398a44e1
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-11-29 19:33:14 +0000

    format code + add link to flow icon

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,12 +3,20 @@ import { Link } from "react-router-dom";
 function LandingPage() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <h1 className="md:text-5xl sm:text-4xl text-3xl text-white mb-10 font-bold">Data Visualization Hub</h1>
+      <h1 className="md:text-5xl sm:text-4xl text-3xl text-white mb-10 font-bold">
+        Data Visualization Hub
+      </h1>
       <div className="flex">
-        <Link className="transform transition bg-blue-600 hover:bg-blue-800 text-white font-bold py-3 px-6 m-4 rounded-lg shadow-lg hover:scale-110 hover:text-white hover:no-underline" to="/volcano-plot">
+        <Link
+          className="transform transition bg-blue-600 hover:bg-blue-800 text-white font-bold py-3 px-6 m-4 rounded-lg shadow-lg hover:scale-110 hover:text-white hover:no-underline"
+          to="/volcano-plot"
+        >
           Volcano Plot
         </Link>
-        <Link className="transform transition bg-purple-600 hover:bg-purple-800 text-white font-bold py-3 px-6 m-4 rounded-lg shadow-lg hover:scale-110 hover:text-white hover:no-underline" to="/pca-plot">
+        <Link
+          className="transform transition bg-purple-600 hover:bg-purple-800 text-white font-bold py-3 px-6 m-4 rounded-lg shadow-lg hover:scale-110 hover:text-white hover:no-underline"
+          to="/pca-plot"
+        >
           PCA Plot
         </Link>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,27 @@
-import Flow from '../assets/flow.png'
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import BackButton from './BackButton';
+import Flow from "../assets/flow.png";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import BackButton from "./BackButton";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    const location = useLocation();
-    const [showBackButton, setShowBackButton] = useState(false);
-  
-    useEffect(() => {
-      // Update this condition based on your requirement
-      const shouldShowBack = location.pathname !== '/';
-      setShowBackButton(shouldShowBack);
-    }, [location]);
+  const location = useLocation();
+  const [showBackButton, setShowBackButton] = useState(false);
 
-    return (
-        <div className="flex items-center justify-between drop-shadow w-full px-4 py-1 bg-slate-100">
-            <img src={Flow} className="h-16 w-18" alt="Flow logo" />
-            {showBackButton && <BackButton />}
-        </div>
-    )
+  useEffect(() => {
+    // Update this condition based on your requirement
+    const shouldShowBack = location.pathname !== "/";
+    setShowBackButton(shouldShowBack);
+  }, [location]);
+
+  return (
+    <div className="flex items-center justify-between drop-shadow w-full px-4 py-1 bg-slate-100">
+      <Link className="focus:outline-none" to="/">
+        <img src={Flow} className="h-16 w-18" alt="Flow logo" />
+      </Link>
+      {showBackButton && <BackButton />}
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
