065d78eb607ad1b586fb0ed39e8237a7187ab41f
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-11-29 19:48:01 +0000

    increase size of buttons

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,15 +6,15 @@ function LandingPage() {
       <h1 className="md:text-5xl sm:text-4xl text-3xl text-white mb-10 font-bold">
         Data Visualization Hub
       </h1>
-      <div className="flex">
+      <div className="flex text-center">
         <Link
-          className="transform transition bg-blue-600 hover:bg-blue-800 text-white font-bold py-3 px-6 m-4 rounded-lg shadow-lg hover:scale-110 hover:text-white hover:no-underline"
+          className="transform transition bg-blue-600 hover:bg-blue-800 text-white font-bold py-3 px-6 m-4 w-36 rounded-lg shadow-lg hover:scale-110 hover:text-white hover:no-underline"
           to="/volcano-plot"
         >
           Volcano Plot
         </Link>
         <Link
-          className="transform transition bg-purple-600 hover:bg-purple-800 text-white font-bold py-3 px-6 m-4 rounded-lg shadow-lg hover:scale-110 hover:text-white hover:no-underline"
+          className="transform transition bg-purple-600 hover:bg-purple-800 text-white font-bold py-3 w-36 px-6 m-4 rounded-lg shadow-lg hover:scale-110 hover:text-white hover:no-underline"
           to="/pca-plot"
         >
           PCA Plot
